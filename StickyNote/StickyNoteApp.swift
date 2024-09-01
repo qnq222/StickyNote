9a52d027a90eb0329b23f48b6b931f035954c1dd
Ayman A. AbuMutair <qnq222@gmail.com>
2024-09-01 09:05:44 +0300

    Initial Commit

---
+++ StickyNote/StickyNoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  StickyNoteApp.swift
+//  StickyNote
+//
+//  Created by Ayman AbuMutair on 01/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct StickyNoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
